ad6fdc6386628a600e3474bef41399b0b2386508
OshidhieP <oshidhiep@lolctech.com>
2024-02-27 14:03:06 +0530

    WebDriverManager added

--- src/main/java/com/example/webdriver/DriverInitialization.java
+++ src/main/java/com/example/webdriver/DriverInitialization.java
@@ -1,11 +1,13 @@
 package com.example.webdriver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DriverInitialization {
 
     public static WebDriver initializeDriver() {
+        WebDriverManager.chromedriver().setup();
         return new ChromeDriver();
     }
 }
